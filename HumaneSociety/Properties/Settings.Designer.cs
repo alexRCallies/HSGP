7677abe29b54629f8a0c5e4e60891fdb93f44303
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-02-10 09:19:10 -0600

    database linkjed

--- HumaneSociety/Properties/Settings.Designer.cs
+++ HumaneSociety/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace HumaneSociety.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-3B575C3\\SQLEXPRESS;Initial Catalog=HumaineSpciety;Integrated " +
+            "Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        public string HumaineSpcietyConnectionString {
+            get {
+                return ((string)(this["HumaineSpcietyConnectionString"]));
+            }
+        }
     }
 }
